1e0d53d9e432b9769488df096dbc05501a71e860
bryam <bryampinos30@gmail.com>
2024-03-30 23:29:54 -0500

    Eliminar todo el contenido de la rama

--- router/repreRouter.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const repreController = require('../controller/repreController');
-
-const router = express.Router();
-
-router.post('/register', repreController.register);
-router.post('/login', repreController.login);
-
-module.exports = router;
