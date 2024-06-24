70ccef617a1db855e75255c46038bb4f0493bb26
bryam <bryampinos30@gmail.com>
2024-06-24 16:26:54 -0500

    cambios de dependencias

--- services/userService.js
+++ services/userService.js
@@ -1,4 +1,4 @@
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 require ('dotenv').config();
 const jwt = require('jsonwebtoken');
 const userRepository = require('../repository/userRepository');
