77e5ad1d8b50fc4735efbc15b2bdae4caca2e2d1
bryam <bryampinos30@gmail.com>
2024-06-24 18:20:04 -0500

    sad

--- models/docente.js
+++ models/docente.js
@@ -1,7 +1,7 @@
 // models/Docente.js
 const { DataTypes } = require('sequelize');
 const sequelize = require('../config/db');
-
+const User = require('./user');
 
 
 const Docente = sequelize.define('docente', {
@@ -12,7 +12,7 @@ const Docente = sequelize.define('docente', {
   user_iduser: {
     type: DataTypes.STRING,
     references: {
-      model: 'user',
+      model: User,
       key: 'iduser'
     }
   }
